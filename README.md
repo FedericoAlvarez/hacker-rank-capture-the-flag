1f4299699778852fbaf8d02d5351b0b41644acf1
FedericoAlvarez <federico.n.alvarez@gmail.com>
2017-05-02 00:14:32 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Capture the flag
+3rd exercise from https://www.hackerrank.com/contests/capture-the-flag
---
+++ capture-the-flag-3rd.rb
@@ -0,0 +1,44 @@
+require 'nokogiri'
+require 'open-uri'
+require 'set'
+
+@links = Set.new
+@secretsWords = Set.new
+
+class HackerRankURL
+  @document
+  @url
+
+  def initialize(url)
+    @document = Nokogiri::HTML(open("http://cdn.hackerrank.com/hackerrank/static/contests/capture-the-flag/infinite/#{url}.html"))
+    @url = url
+  end
+
+  def getLinks()
+    newLinks = @document.xpath("//a")
+    return newLinks
+  end
+
+  def getSecretWord()
+    return @document.css("font")[0].content
+  rescue
+  end
+
+  def getURL()
+    return @url
+  end
+
+end
+
+def transverse(web)
+  if !@links.include? web.getURL
+    @links.add web.getURL
+    @secretsWords.add web.getSecretWord
+    web.getLinks.each{|a| transverse(HackerRankURL.new(a.content))}
+  end
+end
+
+web = HackerRankURL.new("qds")
+transverse(web)
+
+@secretsWords.each { |a|  puts a}
